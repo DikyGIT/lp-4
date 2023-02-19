0a8e19eb2ff5df3d1194c41b99974de454866e36
DikyGIT <dikyoga27@gmail.com>
2023-02-19 15:14:45 +0700

    landing page 4

---
+++ dist/js/script.js
@@ -0,0 +1,24 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  menu.classList.toggle("menu-active");
+});
+
+window.addEventListener("scroll", () => {
+  hamburger.classList.remove("is-active");
+  menu.classList.remove("menu-active");
+});
+
+const loader = document.querySelector(".loader");
+
+function loaderActive() {
+  loader.classList.add("loader-active");
+}
+
+function loaderActiveTime() {
+  setInterval(loaderActive, 3000);
+}
+
+window.onload = loaderActiveTime();
